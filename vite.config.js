0cf0887a3c77f971c231c1ddcf6bff45195e481b
YourPalZack <zachariahthegreat@gmail.com>
2025-01-25 17:22:20 -0700

    Update build and routing configuration

--- vite.config.js
+++ vite.config.js
@@ -9,16 +9,22 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    sourcemap: true,
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
+      input: resolve(__dirname, 'index.html'),
       output: {
-        manualChunks: undefined
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
       }
     }
   },
   server: {
     port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
   }
 }) 
